bcbc94462e8c87492c004663e96fce8177a4a3e5
Matthew Wampler-Doty <negacthulhu@gmail.com>
2011-08-07 14:54:08 -0500

    find_clusters now produces a "clusters" directory containing the catalogue of clusters we are generating

--- find_clusters.py
+++ find_clusters.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import csv,sys
+import csv,sys,os
 import numpy as np
 from math import sqrt,sin,cos,atan2,pi,acos
 from cluster import *
@@ -19,11 +19,10 @@ def Vincenty(phi1,lam1,phi2,lam2) :
 	return atan2(sqrt((cos(phi2)*sin(dlam))**2 + (cos(phi1)*sin(phi2) - sin(phi1)*cos(phi2)*cos(dlam))**2),
                      sin(phi1)*sin(phi2) + cos(phi1)*cos(phi2)*cos(dlam))
 
-# Takes two tsv lines with RA and DEC as columns 1 and 2 
-# and computes great arc length
+# Takes two tsv lines with RA and DEC (in degrees) as columns 1 and 2 
+# and computes great arc length (in Radians)
 def tsvDist(line1,line2) : 
-	return Vincenty(*map((lambda x: degToRad(float(x))),
-                              line1[1:3]+line2[1:3]))
+	return Vincenty(*map((lambda x: degToRad(float(x))), line1[1:3]+line2[1:3]))
 
 # Gets all pairs from a list
 def pairs(L):
@@ -31,7 +30,41 @@ def pairs(L):
 		i = L.pop()
 		for j in L: yield i, j
 
-if __name__ == "__main__":
-	data = csv.reader(open(sys.argv[1]), delimiter='\t')
+# Fixes a list of lists and singletons so it's a list of lists
+def fixLoLHelper(badlol):
+	for x in badlol:
+		if isinstance(x,list): yield x
+		else : yield [x]
+
+def fixLoL(badlol):
+	return list(fixLoLHelper(badlol))
+
+# Write clusters out to directories
+clustersDir="clusters"
+def writeClusters(cls):
+	for c in cls:
+		clusterName=c[0][1]+c[0][2]
+		thisDir=os.path.join(clustersDir,clusterName)
+		if not os.path.exists(thisDir): os.makedirs(thisDir)
+		newfn = os.path.join(thisDir,clusterName+".tsv")
+		f = open(newfn,'w')
+		print >>f, "\n".join(map(lambda x: "\t".join(x),c))
+		print "Wrote:", newfn 
+		f.close()
+		
+# Clusters the entry in a filename
+# Pretty wide
+clusterLvl=.012
+def mkClusters(filename):
+	f = open(filename)
+	data = csv.reader(f, delimiter='\t')
 	cl = HierarchicalClustering(list(data), tsvDist)
-	print len(cl.getlevel(.012))
+	outlol=fixLoL(cl.getlevel(clusterLvl))
+	f.close()
+	return outlol
+	
+if __name__ == "__main__":
+	print "Computing Clusters"
+	cls=mkClusters(sys.argv[1])
+	print "Writing Cluster Directories"
+	writeClusters(cls)
