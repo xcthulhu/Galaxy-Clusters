0fe3d3aaf9b59b57cdad8b14d80ebc8b0f0fb78b
Matthew Wampler-Doty <negacthulhu@gmail.com>
2011-08-08 11:58:12 -0500

    Using new O(n^2) algorithm for clustering (much faster)

--- find_clusters.py
+++ find_clusters.py
@@ -2,9 +2,13 @@
 import csv,sys,os
 import numpy as np
 from math import sqrt,sin,cos,atan2,pi,acos
-from cluster import *
+from fastcluster import *
+from scipy.cluster.hierarchy import to_tree
+from sets import Set
 
-def degToRad(x) : return x/360. * 2 * pi
+def degToRad(x) : return x / 360. * 2 * pi
+
+def radToDeg(x) : return x / (2 * pi) * 360.
 
 # Crappy formula for great circle distance based on law of cosines
 # (for debugging Vincenty formula code - do not use!)
@@ -19,10 +23,31 @@ def Vincenty(phi1,lam1,phi2,lam2) :
 	return atan2(sqrt((cos(phi2)*sin(dlam))**2 + (cos(phi1)*sin(phi2) - sin(phi1)*cos(phi2)*cos(dlam))**2),
                      sin(phi1)*sin(phi2) + cos(phi1)*cos(phi2)*cos(dlam))
 
+# Crazy version of Vincenty formula that takes 2-vectors of degrees as input and outputs radians
+def myVincenty(v1,v2) :
+	return Vincenty(*map(degToRad, [v1[0],v1[1],v2[0],v2[1]]))
+
 # Takes two tsv lines with RA and DEC (in degrees) as columns 1 and 2 
 # and computes great arc length (in Radians)
 def tsvDist(line1,line2) : 
 	return Vincenty(*map((lambda x: degToRad(float(x))), line1[1:3]+line2[1:3]))
+
+# Converts a line of data into a numpy array
+def lineToPair(l) : 
+	return (float(l[1]),float(l[2]))
+
+# Converts derived data from a tsv to an array of (unique) vectors
+def dataToArray(data) :
+	s = Set(map(lineToPair,data))
+	return np.array(list(s))
+
+# Computes the distance matrix from an array of two vectors given a distance function
+def distMat(data,metric) :
+	m = np.zeros((len(data),len(data)))
+	for i in range(len(data)) :
+		for j in range(len(data)) :
+			m[i][j] = metric(data[i],data[j])
+	return m
 
 # Gets all pairs from a list
 def pairs(L):
@@ -39,9 +64,54 @@ def fixLoLHelper(badlol):
 def fixLoL(badlol):
 	return list(fixLoLHelper(badlol))
 
+# Takes a scipy.cluster.hierarchy.ClusterNode (ie, a tree)
+# and returns a lazy list of leaves
+def leaves(cls) :
+	if cls == None : return []
+	if cls.is_leaf() : return [cls.id]
+	else : return leaves(cls.left) + leaves(cls.right) 
+
+# Takes a ClusterNode tree 
+# and returns a list of lists of ids, seperated by the given distance
+# (forms a paritition over the leaves)
+def getLevel(cls,d) : 
+	if cls == None : return []
+	if cls.dist <= d : return [leaves(cls)]
+	else : return getLevel(cls.left, d) + getLevel(cls.right, d)
+
+# Takes the modulus of some data using a partition and a membership function
+def modData(data, part, memF) :
+	l = map(lambda x : [], part)
+	for x in data :
+		for n in range(len(part)) :
+			if memF(x,part[n]) : 
+				l[n].append(x)
+				break
+	return l
+	
+# Clusters the entry in a filename
+# Default is pretty wide
+def mkClusters(filename,clusterLvl=.012) :
+	f = open(filename)
+	data = list(csv.reader(f, delimiter='\t'))
+	vecs = dataToArray(data)
+	tree = to_tree(linkage(vecs, metric=myVincenty))
+	lol = getLevel(tree, clusterLvl)
+	part = [ map(lambda n : vecs[n], x) for x in lol ]
+	memF = lambda x,p : any(map(lambda y : (float(x[1]),float(x[2])) == (y[0],y[1]),p))
+	myCluster = modData(data, part, memF)
+	#x = data[0]
+	#myCluster = [ [ x for y in p if memF(x,y) ] for p in part ]
+	f.close()
+	return map(len,myCluster)
+	#return part
+
+# Computes the Longitude and Latitude pairs from derived cluster
+def clsToLonLat(cls):
+	return [[float(c[1]),float(c[2])] for c in cls ]
+
 # Write clusters out to directories
-clustersDir="clusters"
-def writeClusters(cls):
+def writeClusters(cls,clustersDir="clusters"):
 	for c in cls:
 		clusterName=c[0][1]+c[0][2]
 		thisDir=os.path.join(clustersDir,clusterName)
@@ -51,20 +121,10 @@ def writeClusters(cls):
 		print >>f, "\n".join(map(lambda x: "\t".join(x),c))
 		print "Wrote:", newfn 
 		f.close()
-		
-# Clusters the entry in a filename
-# Pretty wide
-clusterLvl=.012
-def mkClusters(filename):
-	f = open(filename)
-	data = csv.reader(f, delimiter='\t')
-	cl = HierarchicalClustering(list(data), tsvDist)
-	outlol=fixLoL(cl.getlevel(clusterLvl))
-	f.close()
-	return outlol
+
 	
 if __name__ == "__main__":
 	print "Computing Clusters"
-	cls=mkClusters(sys.argv[1])
-	print "Writing Cluster Directories"
-	writeClusters(cls)
+	print mkClusters(sys.argv[1])
+	#print "Writing Cluster Directories"
+	#writeClusters(cls)
