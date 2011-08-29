61a672251c7f57548a684a478ff5c74ee28cfa41
Matthew Wampler-Doty <matt@w-d.org>
2011-08-29 12:28:29 -0500

    Links scripts for getting XMM observations now

--- make_makefiles.sh
+++ make_makefiles.sh
@@ -6,5 +6,8 @@ for i in `find $1 -type d -depth 1 -print` ; do
 	echo "BASEDIR=../.." > $i/Makefile
 	echo 'include $(BASEDIR)/analyze.mk' >> $i/Makefile
 	echo "`basename $i` : " >> $1/Makefile
-	echo "	make -C $i" >> $1/Makefile
+	echo -e "	make -C $i\n" >> $1/Makefile
+	ln -s ../../chandra_process.mk $i
+	ln -s ../../get_XMM_obs_url.py $i
+	ln -s ../../get_XMM_obs.sh $i
 done
