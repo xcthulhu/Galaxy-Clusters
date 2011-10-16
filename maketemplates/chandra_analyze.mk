d16384c5d3ff0636d42131913c0ce832b7405182
Matthew Wampler-Doty <matt@w-d.org>
2011-10-16 17:50:30 -0500

    Now downloading using chandra-obs/Makefile

--- maketemplates/chandra_analyze.mk
+++ maketemplates/chandra_analyze.mk
@@ -1,7 +1,7 @@
 include $(RAWBASEDIR)/maketemplates/master.mk
 CIAODIR=/usr/local/ciao-4.3/bin
 CIAO_INIT=source $(CIAODIR)/ciao.bash
-OBSDIR=$(RAWBASEDIR)/chandra-obs
+CHANDRA_OBSDIR=$(RAWBASEDIR)/Data/chandra-obs
 
 .PHONY : all clean Makefile
 .PRECIOUS: $(CHANDRA_OBSDIR) $(CHANDRA_OBSDIR)/%
@@ -11,14 +11,8 @@ all : $(OBSIDS)
 clean :
 	rm -f $(OBSIDS)
 
-$(CHANDRA_OBSDIR)/Makefile :
-	$(MAKE) -C $(dir $(CHANDRA_OBSDIR)) $(notdir $(CHANDRA_OBSDIR))/$(notdir $@)
-
-$(CHANDRA_OBSDIR)/% : $(CHANDRA_OBSDIR)/Makefile
-	@echo ">>> Dowloading Chandra ObsId $(notdir $@) <<<"
-	cd $(dir $@) && \
-	$(CIAO_INIT) && \
-	download_chandra_obsid $(notdir $@)
+$(CHANDRA_OBSDIR)/% :
+	$(MAKE) -C $(CHANDRA_OBSDIR) $(notdir $@)
 
 %: $(CHANDRA_OBSDIR)/% 
 	@echo ">>> Linking Chandra ObsId $(notdir $@) <<<"
