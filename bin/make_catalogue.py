ca48c285ee77aeaa0948b37ca192cbb15da1b892
Matthew Wampler-Doty <matt@w-d.org>
2011-10-16 17:27:33 -0500

    Removed commented out garbage

--- bin/make_catalogue.py
+++ bin/make_catalogue.py
@@ -10,10 +10,3 @@ if __name__ == "__main__":
 	clsDir = sys.argv[2]
 	print ("Writing cluster directories to %s" % clsDir)
 	writeClusters(cls,clsDir)
-        #print ("Culling clusters smaller than %s..." % sys.argv[1])
-        #bigguys = filter(lambda x : len(x) > int(sys.argv[1]), cls)
-        #pts = map(lambda x : list(dataToArray(x)[0]) + [len(x),x[0][7]],bigguys)
-        #print ("Plotting to %s..." % sys.argv[3])
-        #plot_clusters(pts,sys.argv[3],float(sys.argv[1]))
-        #print "Wrote:",sys.argv[3]
-
