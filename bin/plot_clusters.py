7440d654283c823cf76a38938b103c2019482ba6
Matthew Wampler-Doty <matt@w-d.org>
2011-10-20 10:16:22 -0500

    Prettier plot of observation clusters

--- bin/plot_clusters.py
+++ bin/plot_clusters.py
@@ -5,29 +5,44 @@ import matplotlib.pyplot as plt
 import sys
 from math import log,pi
 from find_clusters import mkClusters,dataToArray
-
-import find_clusters # Module in this directory
+from master_variables import ZONE_OF_AVOIDANCE
+import find_clusters 
 
 def r2d(r) : return r / (2*pi) * 360
 
-def plot_clusters(pts,filename,scale):
+def plot_clusters(pts,filename,markerscale,background_img=None):
 	m = Basemap(projection='moll',lon_0=0,resolution='c')
-	m.drawparallels(np.arange(-90.,120.,15.),color='white',linewidth=.5)
+	if background_img: m.warpimage(background_img)
+	else: m.drawmapboundary(fill_color='black')
+	m.drawparallels(np.arange(-90.,180.,15.),color='white',linewidth=.5)
 	m.drawmeridians(np.arange(0.,420.,15.),color='white',linewidth=.5)
-	m.drawmapboundary(fill_color='black')
 	for px,py,sz,name in pts:
 		x,y = m(r2d(px),r2d(py))
-		plt.plot(x,y,'*',color='yellow',markersize=scale*log(sz,scale))
+		plt.plot(x,y,'*',color='yellow',markersize=markerscale*log(sz,markerscale))
 		plt.text(x,y,name,color='red',size=2)
+	# Plot the zone of avoidance
+	m.drawparallels([ZONE_OF_AVOIDANCE,-ZONE_OF_AVOIDANCE], color='r', linewidth=1)
+	x,y=m(0,-(ZONE_OF_AVOIDANCE-2))
+	plt.text(x,y,r'$b = %i^{\circ}$' % (-(ZONE_OF_AVOIDANCE)),color='r',size=7, ha='center', va='bottom')
+	x_,y_=m(0,(ZONE_OF_AVOIDANCE-2))                      
+	plt.text(x_,y_,r'$b = %i^{\circ}$' % (ZONE_OF_AVOIDANCE),color='r',size=7,ha='center', va='top')
 	#plt.title("Galaxy Clusters")
-	plt.savefig(filename)
+	plt.savefig(filename,dpi=300)
 
 if __name__ == "__main__":
 	print "Computing cluster points..."
-	cls = mkClusters(sys.argv[2])
-	print "Culling clusters smaller than", sys.argv[1], "..."
-	bigguys = filter(lambda x : len(x) > int(sys.argv[1]), cls)
+	cls = mkClusters(sys.argv[1])
+	if len(sys.argv) == 5: SZ = int(sys.argv[3])
+	elif len(sys.argv) == 4: SZ = int(sys.argv[2])
+	else : raise "Incorrect command line arguments"
+	print "Culling clusters smaller than", SZ, "..."
+	bigguys = filter(lambda x : len(x) > SZ, cls)
 	pts = map(lambda x : list(dataToArray(x)[0]) + [len(x),x[0][7]],bigguys)
 	print "Making Plot..."
-	plot_clusters(pts,sys.argv[3],float(sys.argv[1]))
-	print "Wrote:",sys.argv[3]
+	if len(sys.argv) == 5: 
+		plot_clusters(pts,sys.argv[4],float(SZ),sys.argv[2])
+		print "Wrote:",sys.argv[4]
+	elif len(sys.argv) == 4: 
+		plot_clusters(pts,sys.argv[3],float(SZ))
+		print "Wrote:",sys.argv[3]
+	else : raise "Incorrect command line arguments"
