b6b8627b15ba391126dbe8b549472efa60e40945
Matthew Wampler-Doty <matt@w-d.org>
2011-11-19 11:35:16 -0600

    Has proper error code now

--- bin/make_test_image.py
+++ bin/make_test_image.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
 import numpy as np
-import sys
+import sys, errno
 from random import uniform
 from scipy.misc import toimage
 
 def make_test_image(shape, freq):
 	"""make_test_image(shape, frequency)
 	
-	   - Outputs: A binary numpy array m where m.shape = shape and
-	     (# of non-zero points)
-	     ---------------------   = freq
-	        total points
-	   And the points are uniformly distributed"""
+	- Outputs: A binary numpy array m where m.shape == shape and
+
+	  (# of non-zero points)
+	   ---------------------   == freq
+	      total points
+	  
+	  the points are uniformly distributed"""
 	def maybe(x) : 
 		if uniform(0,1) < freq : return 1
 		else : return 0
@@ -34,12 +36,14 @@ def get_points(m) :
 def get_dist_hash(m,ndigits = 2):
 	pts = list(get_points(m))
 	h = {}
-		try : h[d].append(set([pt1,pt2]))
-		except : h[d] = [set([pt1,pt2])]
+	try : h[d].append(set([pt1,pt2]))
+	except : h[d] = [set([pt1,pt2])]
 
 if __name__ == '__main__':
+	if len(sys.argv) <= 6:
+		print >>sys.stderr, "Usage: %s x-dimension y-dimension frequency x-translation y-translation" % sys.argv[0]
+		sys.exit(errno.EINVAL)
 	shape = (int(sys.argv[1]),int(sys.argv[2]))
 	trans = (int(sys.argv[4]),int(sys.argv[5]))
 	m = make_test_image(shape,float(sys.argv[3]))
 	toimage(translate(m,trans)).show()
-
