ff878682a39fcfcd9e8aa8a935d5fb0dfe0ae2ae
Matthew Wampler-Doty <matt@w-d.org>
2011-11-19 10:55:50 -0600

    Draft of XMM processing system

--- maketemplates/master.mk
+++ maketemplates/master.mk
@@ -5,3 +5,8 @@ BIN=$(BASEDIR)/bin
 
 CIAODIR=/usr/local/ciao-4.3/bin
 CIAO_INIT=source $(CIAODIR)/ciao.bash -o
+
+SRC_CLSTR_RADIUS=3
+
+XMMDIR=/usr/local/XMM/xmmsas_20110223_1803/
+XMM_INIT=source $(XMMDIR)/
