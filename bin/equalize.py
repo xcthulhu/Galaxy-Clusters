89c738fb954584b02d49e8f09c35d0cf33704028
Matthew Wampler-Doty <matt@w-d.org>
2011-11-19 10:53:10 -0600

    slightly faster now

--- bin/equalize.py
+++ bin/equalize.py
@@ -2,12 +2,10 @@
 import numpy as np
 
 def histeq(im,nbr_bins=256):
-
    # Get image histogram
    imhist,bins = np.histogram(im.flatten(),nbr_bins,normed=True)
    cdf = imhist.cumsum() #cumulative distribution function
-   cdf_fixed = ((np.max(im) - np.min(im)) * (cdf - np.min(cdf)) / np.max(cdf - np.min(cdf))) + np.min(im) #normalize
-   cdf_fixed[0] = np.min(im)
+   cdf_fixed = ((np.max(im) - np.min(im)) * (cdf - cdf[0]) / (cdf[-1] - cdf[0]) + np.min(im) #normalize
 
    # Use linear interpolation of cdf to find new pixel values
    im2 = np.interp(im.flatten(),bins[:-1],cdf)
