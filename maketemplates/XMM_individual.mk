a78e83412d507560c5cf976315213a3023f59044
Matthew Wampler-Doty <matt@w-d.org>
2011-11-19 10:53:32 -0600

    Rough draft at processing system for XMM data

--- maketemplates/XMM_individual.mk
+++ maketemplates/XMM_individual.mk
@@ -1,3 +1,19 @@
 include $(RAWBASEDIR)/maketemplates/master.mk
 
-all : 
+GZMOS1=pps/*M1*EVL*.FTZ
+GZMOS2=pps/*M2*EVL*.FTZ
+GZPN=pps/*M2*EVL*.FTZ
+
+all : $(GZMOS1) $(GZMOS2) $(GZPN)
+
+mos1.fits : $(GZMOS1)
+	gzcat $^ > $@
+
+mos2.fits : $(GZMOS2)
+	gzcat $^ > $@
+
+pn.fits : $(GZPN)
+	gzcat $^ > $@
+
+clean :
+	rm -f *.fits
