5a48f20ca6e5dedc41ef4b7b9d0f721dddc064b0
Matthew Wampler-Doty <matt@w-d.org>
2011-12-12 12:53:27 -0600

    - Using command line option to filter agglomerations - Using ruby to clean up dirty filenames and acquire dirnames

--- bin/chronicle_galaxy_clusters_according_to_ned.sh
+++ bin/chronicle_galaxy_clusters_according_to_ned.sh
@@ -1,11 +1,15 @@
 #!/bin/sh -x
 
+echo $1 $2
 # Make sure you have created all of the necessary nedshifts.tsv files before running
-for i in `find $1 -iname "nedshifts.tsv"` ; do 
-	if [ "`grep -i 'GClstr' $i`" ] ; then 
-		IDIRNAME=`dirname $i` 
+for i in `find $2 -iname "nedshifts.tsv"` ; do 
+	NED=`grep GClstr $i | cut -f1,6 | tr "\n" "\t"`
+	if [ -n "${NED}" ] ; then 
+		IDIRNAME=`ruby -e "require 'pathname' ; puts Pathname.new(\"$i\").dirname.cleanpath"`
 		OBS="${IDIRNAME}/${IDIRNAME}.tsv"
-		NED=`grep GClstr $i | cut -f1,6`
-		echo `wc -l ${OBS}` "	" `head -2 ${OBS} | cut -f 8` "	" $NED
+		IDCOUNT=`wc -l ${OBS} | cut -f1 -d" "`
+		if [ ${IDCOUNT} -ge $1 ] ; then
+			echo ${IDCOUNT} "	" $OBS "	" `head -2 ${OBS} | cut -f 8` "	" $NED
+		fi
 	fi 
 done
