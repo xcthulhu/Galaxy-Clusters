2acf8c6fc70686c1b0171abd04e6064c94744603
Matthew Wampler-Doty <matt@w-d.org>
2012-01-04 15:22:52 -0600

    Properly finds candidates for analysis using retrieved entries from the ned database

--- bin/chronicle_galaxy_clusters_according_to_ned.sh
+++ bin/chronicle_galaxy_clusters_according_to_ned.sh
@@ -1,14 +1,13 @@
 #!/bin/sh -x
 
-echo $1 $2
 # Make sure you have created all of the necessary nedshifts.tsv files before running
-for i in `find $2 -iname "nedshifts.tsv"` ; do 
-	NED=`grep GClstr $i | cut -f1,6 | tr "\n" "\t"`
+for i in `find $4 -iname "nedshifts.tsv"` ; do 
+	NED=`grep GClstr $i | cut -f1,6 | awk -F"\t" "$1 <= \\\$2 && \\\$2 <= $2 {print \\\$0}" | tr "\t\n" ",;"`
 	if [ -n "${NED}" ] ; then 
 		IDIRNAME=`ruby -e "require 'pathname' ; puts Pathname.new(\"$i\").dirname.cleanpath"`
 		OBS="${IDIRNAME}/${IDIRNAME}.tsv"
 		IDCOUNT=`wc -l ${OBS} | cut -f1 -d" "`
-		if [ ${IDCOUNT} -ge $1 ] ; then
+		if [ ${IDCOUNT} -ge $3 ] ; then
 			echo ${IDCOUNT} "	" $OBS "	" `head -2 ${OBS} | cut -f 8` "	" $NED
 		fi
 	fi 
