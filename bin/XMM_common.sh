984356e6ff1fa78b084f45d94d32a5df23eca871
Matthew Wampler-Doty <matt@w-d.org>
2012-03-29 19:12:56 -0500

    Logic for determining current working directory

--- bin/XMM_common.sh
+++ bin/XMM_common.sh
@@ -1,5 +1,33 @@
 #!/bin/sh
 
+
+function expand() {
+	ruby -e "puts File.expand_path(\"$1\")"
+}
+
+BIN_DIR="$(dirname "${BASH_SOURCE[0]}")"
+if ([ ! ${BIN} ] || [ ! -d ${BIN} ]) && [ -d ${BIN_DIR} ] ; then 
+	export BIN=`expand ${BIN_DIR}`
+fi
+
+if [[ ! "${PATH}" =~ "${BIN}" ]] ; then
+	export PATH=${BIN}:${PATH}
+fi
+
+export DATRED=`expand .` 
+
+if ([ ! ${SAS_ODF} ] || [ ! -d ${SAS_ODF} ]) && [ -d ../odf ] ; then
+	export SAS_ODF=`expand ../odf`
+fi
+
+if ([ ! ${SAS_CCF} ] || [ ! -d ${SAS_CCF} ]) ; then 
+	if [ -e ccf.cif ] ; then
+		export SAS_CCF=`expand ccf.cif`
+	elif [ -e ../work/ccf.cif ] ; then 
+		export SAS_CCF=`expand ../work/ccf.cif`
+	fi
+fi
+
 if [ -d /usr/local/heasoft-6.4.1/x86_64-unknown-linux-gnu-libc2.5/ ] ; then
         export HEADAS="/usr/local/heasoft-6.4.1/x86_64-unknown-linux-gnu-libc2.5/"
 elif [ -d /usr/share/heasoft/heasoft-6.10/i386-apple-darwin9.8.0 ] ; then
@@ -20,3 +48,5 @@ else
         exit 1
 fi
 
+source $HEADAS/headas-init.sh
+source $SAS_DIR/setsas.sh
