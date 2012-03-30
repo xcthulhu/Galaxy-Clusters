38974943abfb6945d7a205190f5a1288a9b39923
Matthew Wampler-Doty <matt@w-d.org>
2012-03-29 19:15:26 -0500

    Source detection routines

---
+++ bin/mos1_source_detect.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -x
+
+export EMLLIST=$1
+export EVENTSETS=$2
+export ATTITUDESET=$3
+export IMAGESETS=${@:4}
+
+BIN_DIR="$(dirname "${BASH_SOURCE[0]}")"
+source ${BIN_DIR}/XMM_common.sh
+
+edetect_chain imagesets="${IMAGESETS}" eventsets=${EVENTSETS} attitudeset=${ATTITUDESET} pimin='200 500 1000 2000 4500' pimax='500 1000 2000 4500 12000' ecf='1.772 1.977 0.745 0.277 0.030' eboxm_list="M_${EMLLIST}" eboxl_list="L_${EMLLIST}" eml_list="${EMLLIST}" esen_mlmin=15 esp_nsplinenodes=16
