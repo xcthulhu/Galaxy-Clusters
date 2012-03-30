08110a844c01ee2f7a5953906989c99c3f35a89d
Matthew Wampler-Doty <matt@w-d.org>
2012-03-29 19:17:05 -0500

    sourcelist dumper

---
+++ bin/dump_XMM_sourcelist.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import pyfits
+import os.path
+import sys
+import numpy as np
+
+def print_sources(filename):
+    """Prints the sources in filename to standard out"""
+    if os.path.isfile(filename):
+        srcs = pyfits.open(filename)
+        for source in srcs['srclist'].data:
+            print source['ra'], '\t', source['dec']
+        srcs.close()
+
+if __name__ == "__main__":
+    map(print_sources,sys.argv[1:])
