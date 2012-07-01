250343baf36e2f14814509ce19801a07fd87bbf8
Matthew Wampler-Doty <matt@w-d.org>
2012-07-01 08:43:23 -0500

    Simplifying python logic

--- maketemplates/python_logic.mk
+++ maketemplates/python_logic.mk
@@ -1,13 +1,1 @@
-PYTHON=python
-
-ifeq ("$(shell [ -e /opt/local/bin/python2.7 ] && echo -n okay)","okay")
-  PYTHON=/opt/local/bin/python2.7
-endif
-
-ifeq ("$(shell sw_vers | grep 'ProductVersion:' | grep -o '[0-9]*\.[0-9]*\.[0-9]*' | cut -d'.' -f1,2)", "10.6")
-  PYTHON=source $(BASEDIR)/venv/bin/activate && python
-endif
-
-ifeq ("$(shell [ -e /etc/redhat-release ] && echo -n okay)","okay")
-  PYTHON=python2.6
-endif
+PYTHON=source $(BASEDIR)/venv/bin/activate && python
