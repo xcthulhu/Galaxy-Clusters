368b7a46dc99e0003cfcebcf9d826febb54e49a9
Matthew Wampler-Doty <matt@w-d.org>
2012-07-01 08:44:41 -0500

    Simplifying logic to determine architecture

--- maketemplates/master.mk
+++ maketemplates/master.mk
@@ -4,14 +4,8 @@ include $(BASEDIR)/maketemplates/python_logic.mk
 BIN:=$(BASEDIR)/bin
 BIN=$(BASEDIR)/bin
 
-ifeq ("$(shell sw_vers | grep 'ProductVersion:' | grep -o '[0-9]*\.[0-9]*\.[0-9]*' | cut -d'.' -f1,2)", "10.6")
-  CIAODIR=$(BASEDIR)/ciao-4.4/bin
-else
-  CIAODIR=/usr/local/ciao-4.3/bin
-endif
-
+CIAODIR=$(BASEDIR)/ciao-4.4/bin
 CIAO_INIT=source $(CIAODIR)/ciao.bash -o
-
 CIAO_VER=$(shell $(CIAO_INIT) &> /dev/null && ciaover | head -1 | cut -d' ' -f2)
 
 SRC_CLSTR_RADIUS=3 #UNITS: Arcminutes
