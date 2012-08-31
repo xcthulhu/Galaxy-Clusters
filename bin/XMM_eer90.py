60d75743398dae72c8494e5dc69b566522b93cfa
Matthew Wampler-Doty <matt@w-d.org>
2012-08-31 15:32:19 -0500

    Computing MOS 90% encircling energy correctly now

--- bin/XMM_eer90.py
+++ bin/XMM_eer90.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import sys
-from math import pi
+from numpy import pi,log
 from find_clusters import Vincenty
 
 # MOS detector
@@ -8,31 +8,32 @@ def mos1_5(d):
     """Returns XMM's 90% encircling energy radius (in arcseconds)
        for distance d (in arcminutes) from the boresight axis
        for a point source of 1.5 keV on the MOS detector"""
-    return 28.61381197992193 -  7.917135865709901 * x + (5.290485745213079 + 1.6205710004634912 * x) * log(47.6667 + 39.94444166666666 * x)
+    return 28.61381197992193 -  7.917135865709901 * d + (5.290485745213079 + 1.6205710004634912 * d) * log(47.6667 + 39.94444166666666 * d)
 
 def mos5(d):
     """Returns XMM's 90% encircling energy radius (in arcseconds)
-       for distance d (in arcminutes) from the boresight axis
+       for distance d (in arcminutes) from the boresight adis
        for a point source of 5 keV on the MOS detector"""
-    return -7471.449668276366 -  6324.677785146672 * x + 3.89988429779949 * x**2 + (-80.31135326312163 - 894.8657491650612 * x) * log(47.6667 + 39.94444166666666 * x) +  (5792.36526696159 + 6360.1796030529895 * x) * log(log(47.6667 + 39.94444166666666 * x)) 
+    return -7471.449668276366 -  6324.677785146672 * d + 3.89988429779949 * d**2 + (-80.31135326312163 - 894.8657491650612 * d) * log(47.6667 + 39.94444166666666 * d) +  (5792.36526696159 + 6360.1796030529895 * d) * log(log(47.6667 + 39.94444166666666 * d)) 
 
 def mos9(d):
     """Returns XMM's 90% encircling energy radius (in arcseconds)
        for distance d (in arcminutes) from the boresight axis
        for a point source of 9 keV on the MOS detector"""
-    return 70.25687576209097 + 16.672833325598635 * x + 0.32696358995439484 * x**2 + (-6.3404116283120855 - 2.30587979355479 * x) * log(47.6667 + 39.94444166666666 * x)
+    return 70.25687576209097 + 16.672833325598635 * d + 0.32696358995439484 * d**2 + (-6.3404116283120855 - 2.30587979355479 * d) * log(47.6667 + 39.94444166666666 * d)
 
 def mos_eer90(e,d):
     """Returns XMM's 90% encircling energy radius (in arcseconds)
        for distance d (in arcminutes) from the boresight axis
        for a point source with energy e (in keV) on the MOS detector"""
     # Interpolate the various 90% encircling energy radii
-    if (e <= 1.5) : return mos1_5(d)
-    elif (1.5 <= e <= 5) : return mos1_5(d) * (e - 5) / (1.5 - 5)
-                                + mos5(d) * (e - 1.5) / (5 - 1.5)
-    elif (5 <= e <= 9) : return mos5(d) * (e - 9.) / (5 - 9.)                 
-                              + mos9(d) * (e - 5.) / (9 - 5.)                 
-    else : return mos9(d)
+    if (0 <= d <= 12) :
+	    if (e <= 1.5) : return mos1_5(d)
+	    elif (1.5 <= e <= 5.) : return mos1_5(d) * (e - 5.) / (1.5 - 5.) + mos5(d) * (e - 1.5) / (5 - 1.5)
+	    elif (5. <= e <= 9.) : return mos5(d) * (e - 9.) / (5 - 9.) + mos9(d) * (e - 5.) / (9 - 5.)                 
+	    else : return mos9(d)
+    else :
+	    return -1.
 
 # PNS detector
 
@@ -59,21 +60,16 @@ def pns_eer90(e,d):
        for distance d (in arcminutes) from the boresight axis
        for a point source with energy e (in keV) on the PNS detector"""
     # Interpolate the various 90% encircling energy radii
-    if (e <= 1.5) : return pns1_5(d)
-    elif (1.5 <= e <= 5) : return pns1_5(d) * (e - 5) / (1.5 - 5)
-                                + pns5(d) * (e - 1.5) / (5 - 1.5)
-    elif (5 <= e <= 9) : return pns5(d) * (e - 9.) / (5 - 9.)
-                              + pns9(d) * (e - 5.) / (9 - 5.)
-    else : return pns9(d)
+    if (0 <= d <= 12) :
+	    if (e <= 1.5) : return pns1_5(d)
+	    elif (1.5 <= e <= 5) : return pns1_5(d) * (e - 5) / (1.5 - 5) + pns5(d) * (e - 1.5) / (5 - 1.5)
+	    elif (5 <= e <= 9) : return pns5(d) * (e - 9.) / (5 - 9.) + pns9(d) * (e - 5.) / (9 - 5.)
+	    else : return pns9(d)
+    else :
+	    return -1
 
 # Compute Distance from a FITS file
-def fits_dist(ra, dec, fits_fn):
+def fits_dist(ra, dec, ra_nom, dec_nom):
     """Takes RA/DEC coordinates in degrees, along with an XMM fits file
        returns the distance from the boresight axis in arcminutes"""
-    f = pyfits.open(fits_fn) # *.evt file
-    ra_nom = f['EVENTS'].header['RA_NOM'] # Nominal boresight RA in
- degs
-    dec_nom = f['EVENTS'].header['DEC_NOM'] # Nominal boresight DEC
- in degs
-    return pi/10800. * Vincenty((pi/180.*ra,pi/180.*dec),
-                                (pi/180.*ra_nom,pi/180.*dec_nom))
+    return 10800. / pi * Vincenty((pi/180.*ra,pi/180.*dec),(pi/180.*ra_nom,pi/180.*dec_nom))
